a1764edb22a541cb85b63411ab0c3fdfb0336729
Timo Vijn <timovijn@gmail.com>
2020-10-15 10:59:18 +0200

    Update

--- Elbas volumes/import_elbas.py
+++ Elbas volumes/import_elbas.py
@@ -1,4 +1,3 @@
-
 import os
 os.system('clear')
 import cv2
@@ -15,7 +14,17 @@ from scipy.spatial.distance import pdist, squareform
 import seaborn
 import pandas as pd
 
+os.chdir("./Elbas volumes")
 df = pd.read_html("./elbas-volumes_2019_hourly.xls", header=[2,3], decimal=',', thousands='.')
 df = df[0]
 df = df[['NL']]
 df.to_csv("./csv.csv", index=False)
+print(df)
+
+from matplotlib.pyplot import figure
+figure(num=None, figsize=(16, 12), dpi=80, facecolor='w', edgecolor='k')
+
+df.plot(legend=False)
+# plt.axis('off')
+plt.show()
+# plt.xticks(np.arange(min(df), max(df)+1, 1000))
